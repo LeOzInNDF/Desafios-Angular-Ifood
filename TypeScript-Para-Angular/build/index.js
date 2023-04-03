8e43f36d725f7cc4da175930b6e606842d86bd6c
LeOzInNDF <leo_albano@hotmail.com>
2023-04-02 22:18:56 -0300

    update code

--- TypeScript-Para-Angular/build/index.js
+++ TypeScript-Para-Angular/build/index.js
@@ -6,6 +6,20 @@ const bot1 = {
 const bot2 = {
     id: 1,
     name: "megaman",
+    sayHello: function () {
+        throw new Error("Function not implemented.");
+    }
 };
 console.log(bot1);
 console.log(bot2);
+class pessoa {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+    sayHello() {
+        return `hello i´m ${this.name}`;
+    }
+}
+const p = new pessoa(1, "gutsman");
+console.log(p.sayHello());
---
+++ TypeScript-Para-Angular/src/quando-usar-interfaces.ts
@@ -0,0 +1,43 @@
+//interfaces (type x interface)
+type robot = {
+  readonly  id: number | string;
+  name: string;
+};
+
+interface robot2 {
+  readonly id: number | string;
+  name: string;
+  sayHello(): string;
+}
+
+const bot1: robot = {
+  id: 1,
+  name: "megaman",
+};
+
+const bot2: robot2 = {
+  id: 1,
+  name: "megaman",
+  sayHello: function (): string {
+    throw new Error("Function not implemented.");
+  }
+};
+
+console.log(bot1);
+console.log(bot2);
+
+class pessoa implements robot2 {
+  id: string | number;
+  name: string;
+
+  constructor(id: string | number, name: string) {
+this.id = id;
+this.name = name;
+  }
+sayHello(): string {
+    return `hello i´m ${this.name}`;
+}
+}
+
+const p = new pessoa(1, "gutsman");
+console.log(p.sayHello());
